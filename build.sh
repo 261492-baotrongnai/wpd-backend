1e877ac733735fb8b17614c7bce5ad1d3108e799
ApichayaUWU <apichaya.inkhiewsai@gmail.com>
2025-04-05 11:16:12 +0700

    add : github workflows

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+TAG=${1:-latest} 
 # Load .env file
 # export $(grep -v '^#' .env.production | xargs)
 
 # Build Docker image
 docker buildx build \
   --platform linux/arm64,linux/amd64 \
+  -t apichayauwu/wpd-backend:$TAG \
   -t apichayauwu/wpd-backend:latest --push .
