7d2201e3e10e26a2da5de0a2b3b677e870f4ab7e
ApichayaUWU <apichaya.inkhiewsai@gmail.com>
2025-05-19 03:27:13 +0700

    fix : update CORS configuration to use FRONTEND_URL from ConfigService

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import { ConfigService } from '@nestjs/config'; // Import the 'ConfigService' from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.enableCors({
-    origin: '*',
+    origin: [configService.get<string>('FRONTEND_URL')],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
