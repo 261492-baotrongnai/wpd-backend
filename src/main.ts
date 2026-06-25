a15ea288722e9a28b94f74c464e5a11ba6a6d397
ApichayaUWU <apichaya.inkhiewsai@gmail.com>
2026-06-25 17:44:50 +0700

    refactor : update Gemini model version and log environment variable during bootstrap

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,7 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 8000, '0.0.0.0');
   console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Environment: ${process.env.GEMINI_API_KEY}`);
 }
 void (async () => {
   try {
